3fca8538e50fc1a20aeabf48935fcf7d080ef1a9
Mantas Mikaitis <m.mikaitis@leeds.ac.uk>
2025-03-20 14:26:14 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
 
 sudo apt update
-sudo apt install -y software-properties-common
-sudo add-apt-repository -y ppa:apptainer/ppa
-sudo apt update
-sudo apt install -y apptainer
-
-singularity build xv6-tools-container.sif xv6-tools-container.def
+sudo apt install -y gcc-riscv64-linux-gnu qemu-system-riscv64
